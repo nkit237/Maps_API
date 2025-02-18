a6fe1d65d319208238e7c34db85ccff805689154
nkit <nkit237@yandex.ru>
2025-02-18 15:47:14 +0500

    task 2

--- main.py
+++ main.py
@@ -7,10 +7,16 @@ from PIL import Image
 from yandex_map_library import *
 from distance import lonlat_distance
 
+
 class MapApp:
     def __init__(self, size):
         self.screen = pygame.display.set_mode(size)
-        bytes_img = get_static_map(ll='123,45', z=5)
+        self.z = 10
+        self.coord = [123, 45]
+        self.update_map()
+
+    def update_map(self):
+        bytes_img = get_static_map(ll=','.join(map(str, self.coord)), z=self.z)
         img = pygame.image.load(BytesIO(bytes_img))
         self.screen.blit(img, (0, 0))
 
@@ -20,10 +26,16 @@ class MapApp:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     exit(0)
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_PAGEUP:
+                        self.z = min(21, self.z + 1)
+                    if event.key == pygame.K_PAGEDOWN:
+                        self.z = max(0, self.z - 1)
+                    self.update_map()
 
             pygame.display.flip()
+
 
 if __name__ == '__main__':
     app = MapApp((600, 450))
     app.run()
-
